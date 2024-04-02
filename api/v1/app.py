9761f2532b1d2500b51643591e796b2d9d3995b6
PimehT <tareerepimeh@gmail.com>
2024-04-02 01:20:32 +0100

    task 3: status of my API, task 2: incomplete

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module for app.py """
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """ close storage """
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 page """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", default="0.0.0.0")
+    port = getenv("HBNB_API_PORT", default="5000")
+    app.run(host=host, port=port, threaded=True)
