0ac72dff5d489d9bdbc044df72a8d2a8afe4fae5
PimehT <tareerepimeh@gmail.com>
2024-04-03 02:22:48 +0100

    i dont know what i did

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,27 @@ from flask import jsonify
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    from models import storage
+    from models.state import State
+    from models.city import City
+    from models.amenity import Amenity
+    from models.place import Place
+    from models.review import Review
+    from models.user import User
+
+    classes = {
+        'states': State,
+        'cities': City,
+        'amenities': Amenity,
+        'places': Place,
+        'reviews': Review,
+        'users': User
+    }
+    stats = {}
+    for key, value in classes.items():
+        stats[key] = storage.count(value)
+    return jsonify(stats)
