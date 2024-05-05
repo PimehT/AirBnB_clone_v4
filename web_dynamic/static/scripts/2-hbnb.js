5b9d84e5d69ee2c55dcc67ba5dfd45cae27681d6
PimehT <tareerepimeh@gmail.com>
2024-05-05 22:41:54 +0100

    fix semistandard error

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,25 +1,24 @@
 $(document).ready(function () {
-    const amenities = {};
-    $('input[type="checkbox"]').change(function () {
-      if ($(this).is(':checked')) {
-        amenities[$(this).data('id')] = $(this).data('name');
-      } else {
-        delete amenities[$(this).data('id')];
-      }
-      const names = Object.values(amenities).join(', ');
-      if (names.length > 0) {
-        $('.amenities h4').text(names);
-      } else {
-        $('.amenities h4').html('&nbsp;');
-      }
-    });
-
-    $.get('http://localhost:5001/api/v1/status/', function (data, status) {
-      if (data.status === 'OK') {
-        $('div#api_status').addClass('available');
-      } else {
-        $('div#api_status').removeClass('available');
-      }
-    });
+  const amenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    const names = Object.values(amenities).join(', ');
+    if (names.length > 0) {
+      $('.amenities h4').text(names);
+    } else {
+      $('.amenities h4').html('&nbsp;');
+    }
   });
-  
+
+  $.get('http://localhost:5001/api/v1/status/', function (data, status) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
